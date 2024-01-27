d35af6ef67159f7a0b8163f7f3ec94008807471f
Tonkooo <at.antonio.todorov@gmail.com>
2024-01-27 07:10:09 +0200

    Update views and index.html

--- django_templates_static_files/employees/views.py
+++ django_templates_static_files/employees/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+class Person:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+def index(request):
+    context = {
+        'title': 'Employees list',
+        'person': {
+            'first_name': 'Doncho',
+            'last_name': 'Minkov',
+        },
+        'person_obj': Person("Doncho", "Minkov"),
+
+    }
+
+    return render(request, 'employees/index.html', context)
+
